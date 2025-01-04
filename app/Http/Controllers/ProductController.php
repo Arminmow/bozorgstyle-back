17616410e231f1cc24bb90aba038ce4f9eea9778
Armin <arminmow@gmail.com>
2025-01-04 18:58:44 +0330

    added filter by name and range

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,11 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-class ProductController extends Controller
-{
-    public function index()
-    {
-        $products = Product::all(); // Fetch all products from the database
-        return response()->json($products); // Return the data as JSON
+class ProductController extends Controller {
+    public function index(Request $request) {
+        $query = Product::query();
+
+        // Filter by name ( if provided )
+        if ( $request->has( 'name' ) ) {
+            $query->where( 'name', 'like', '%' . $request->input( 'name' ) . '%' );
+        }
+
+        // Filter by price range ( if provided )
+        if ( $request->has( 'min_price' ) ) {
+            $query->where( 'price', '>=', $request->input( 'min_price' ) );
+        }
+        if ( $request->has( 'max_price' ) ) {
+            $query->where( 'price', '<=', $request->input( 'max_price' ) );
+        }
+
+        // Execute query and get results
+        $products = $query->get();
+
+        return response()->json( $products );
     }
 }
