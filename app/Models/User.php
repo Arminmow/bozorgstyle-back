3b92cc7b2b76390b542ffd5988f947030c5cc16b
Armin <arminmow@gmail.com>
2025-01-17 21:54:44 +0330

    Set up user roles in the database: added migration for roles table, created seeder to insert 'admin' and 'customer' roles, and integrated seeder into DatabaseSeeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,16 +7,15 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
-{
+class User extends Authenticatable implements JWTSubject {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    * The attributes that are mass assignable.
+    *
+    * @var list<string>
+    */
     protected $fillable = [
         'name',
         'email',
@@ -24,35 +23,37 @@ class User extends Authenticatable implements JWTSubject
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+    * The attributes that should be hidden for serialization.
+    *
+    * @var list<string>
+    */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
+    * Get the attributes that should be cast.
+    *
+    * @return array<string, string>
+    */
+    protected function casts(): array {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
 
-    public function getJWTIdentifier()
-    {
+    public function getJWTIdentifier() {
         return $this->getKey();
     }
 
-    public function getJWTCustomClaims()
-    {
+    public function getJWTCustomClaims() {
         return [];
     }
+
+    public function role() {
+        return $this->belongsTo( Role::class );
+    }
+
 }
