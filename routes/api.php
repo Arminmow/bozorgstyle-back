17e1c3beefd92451a49e5e58fe1babdd870f8ea5
Armin <arminmow@gmail.com>
2025-01-17 12:27:05 +0330

    added login route

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,8 @@ Route::get('products/women', [ProductController::class, 'getWomenProducts']);
 
 Route::post('/register', [AuthController::class, 'register']);
 
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware([JwtMiddleware::class])->group(function () {
     Route::get('user', [AuthController::class, 'getUser']);
     Route::post('logout', [AuthController::class, 'logout']);
